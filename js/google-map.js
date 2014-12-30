ab053f7bd2aa0f55cccf87360fc7194787b57fef
Juan Burgos <juan.burgos@live.com>
2014-12-30 18:57:50 -0500

    Change of location on Google Maps
    new lat/long to reflect Florida.
    
    Tally, Daytona, Gville, Orlando, & Tampa all within view

--- js/google-map.js
+++ js/google-map.js
@@ -4,8 +4,8 @@ var map;
 
 function init() {
 	var mapOptions = {
-		center: new google.maps.LatLng(29.7063428,-95.40526,17),
-		zoom: 17,
+		center: new google.maps.LatLng(29.251181, -82.116403),
+		zoom: 7,
 		zoomControl: true,
 		zoomControlOptions: {
 			style: google.maps.ZoomControlStyle.SMALL,
