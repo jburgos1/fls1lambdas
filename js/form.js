2e12dcd96d14acb636bf0dcc012dcbef9d25847d
Jose Mugaburu <mugab686@gmail.com>
2015-01-10 18:42:31 -0500

    Some more code changes

--- js/form.js
+++ js/form.js
@@ -14,223 +14,313 @@ $(document).ready(function(){
     if(!supports.required) {}
     if(!supports.placeholder) {}
 
+ }
 
-    //name Validation
+//Validation of all inputs
+function Validation(){
+
+    //Querying the DOM for input tags
     var name = document.getElementById("name");
     var email = document.getElementById("email");
     var phone = document.getElementById("phone");
     var message = document.getElementById("message");
+    var error = document.getElementById("error");
 
-    //Assert that Button exists
-	var send = document.getElementById("btn-contact");
-
-	if(send) 
-	{
-        send.onclick = function () {
-                this.value = '...Sending';
-                this.style("border-color:#512D1E;");
-                var validate = ValidateEmptyAndNull(name,email,phone,message);
-				if (validate == true){
-					sendForm();
-				}
-				else{
-					var error = document.getElementById("error");
-					error.innerHTML("Your message was not sent. Please fix the fields.");
-				}
-		this.value = 'Send message';
+    if (IsAlphaNumeric(name, 'There was an error with your name')){
+    	if(IsEmail(email, 'Please enter a valid e-mail address')){
+    		if(IsNumeric(phone, 'Please enter only number for your phone number')){
+    			if(MinLength(message,'The message must be at least 100 characters')){
+    				return true;
+    			}
+    		}
+    	}
     }
-
-    function ValidateEmptyAndNull(_name, _email,_phone,_message){
-
-    	//Name Validation
-    	if(_name ==  null || _name.length = 0)
-    	{
-    		error.innerHTML("There is an error with your fields");
-    		return false;
-    	}
-   
-    	//Email Validation
-    	if(_email == null || _email.length = 0)
-		{
-			error.innerHTML("There is an error with your fields");
-			return false;
-		}
-
-    	//Phone Validation
-    	if (_phone == null || _phone.length = 0)
-    	{
-    		error.innerHTML("There is an error with your fields");
-    		return false;
-    	}
-    	//Message
-    	if (_message == null || _message.length = 0 )
-    	{
-    		error.innerHTML("There is an error with your fields");
-    		return false;
-    	}
-
-    	return true;
-    };
-
-    function sendForm()
-    {
-    	error.style("color:green;");
-    	error.innerHTML("Your message was sent");
-    }
+    return false;
+}
 
 
+//Sending the form
+var send = document.getElementById("btn-contact");
+
+if(send) {
+    send.onclick = function () {
+            this.value = '...Sending';
+            this.style("border-color:#512D1E;");
+			sendMessage();
+	this.value = 'Send message';
+	}
+}
+
+
+function IsEmail(email, error){
+	return true;
+};
+
+function IsNumeric(phone, error){
+	var phoneExpression = /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+	if(input.value.match(phoneExpression)){
+		return true;
+	}
+	else{
+		document.getElementById('error').innerHTML = error;
+		phone.focus();
+		phone.style.border = "red";
+		return false;
+	}
+}
+
+function IsAlphaNumeric(name, error){
+	return true;
+}
+
+function MinLength(message, error){
+	return true;
+}
+
+function sendForm(){
+	error.style("color:green;");
+	error.innerHTML("Your message was sent");
+}
 });
 
 /*
-	function validateForm() {
-	  	var valid = 1;
-	  	var email = document.getElementById('email');
-	  	var email_validation = document.getElementById("email_validation");
-	  	var name = document.getElementById('name');
-	  	var name_validation = document.getElementById("name_validation");
-	  	var message_validation = document.getElementById("message_validation");
-	  	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	  
-		if (name.value === "") {
-	    	valid = 0;
-	    	name.style.border = "red";
-	    //name_validation.style.display = "block";
-	    //name_validation.parentNode.style.backgroundColor = "#FFDFDF";
-	  	} else {
-	    //name_validation.style.display = "none";
-	    //name_validation.parentNode.style.backgroundColor = "transparent";
-	  	}
-	  
-	  	if (message.value === "") {
-	    	valid = 0;
-	    //message_validation.innerHTML = "Field Required";
-	    //message_validation.style.display = "block";
-	    //message_validation.parentNode.style.backgroundColor = "#FFDFDF";
-	  	} else {
-	    //message_validation.style.display = "none";
-	    //message_validation.parentNode.style.backgroundColor = "transparent";
-	  	}
-	  
-		if (email.value === "") {
-	    	valid = 0;
-	    //email_validation.innerHTML = "Field Required";
-	    //email_validation.style.display = "block";
-	    //email_validation.parentNode.style.backgroundColor = "#FFDFDF";
-	  	} else {
-	    //email_validation.style.display = "none";
-	    //email_validation.parentNode.style.backgroundColor = "transparent";
-	  	}
-	  
-	  	if(!filter.test(email.value)) {
-	    	valid = 0;
-	    //email_validation.innerHTML = "Invalid email address";
-	    //email_validation.style.display = "block";
-	    //email_validation.parentNode.style.backgroundColor = "#FFDFDF";
-	  	} else {
-	    //email_validation.style.display = "none";
-	    //email_validation.parentNode.style.backgroundColor = "transparent";
-	  	}
+function validateForm() {
+  	var valid = 1;
+  	var email = document.getElementById('email');
+  	var name = document.getElementById('name');
+  	var error = document.getElementById('error');
+  	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  
+	if (name.value === "") {
+    	valid = 0;
+    	name.style.border = "red";
+  	} else {
+ 		error.style.display = "none";
+  	}
+  
+  	if (message.value === "") {
+    	valid = 0;
+		message.style.border = "red";
+  	} else {
+		error.style.display= "none";
+  	}
+  
+	if (email.value === "") {
+    	valid = 0;
+    	email.style.border = "red";
+  	} else {
+    	error.style.display = "none";
+  	}
+  
+  	if(!filter.test(email.value)) {
+    	valid = 0;
+    	
+  	} else {
+    
+  	}
 
-	  	if (!valid)
-	    	return false;
-	}
+  	if (!valid)
+    	return false;
+}
 
-
-	function checkForm() {
-		 name = document.getElementById("name").value;
-		 email = document.getElementById("email").value;
-		 comment = document.getElementById("comment").value;
-	 
-		 if (name == "")
-		 {
-			 hideAllErrors();
-			 document.getElementById("nameError").style.display = "inline";
-			 document.getElementById("name").select();
-			 document.getElementById("name").focus();
-			 return false;
-		 }
-		 else if (email == "")
-		 {
-			 hideAllErrors();
-			 document.getElementById("emailError").style.display = "inline";
-			 document.getElementById("email").select();
-			 document.getElementById("email").focus();
-			 return false;
-		 }
-		 else if (comment == "")
-		 {
-			 hideAllErrors();
-			 document.getElementById("commentError").style.display = "inline";
-			 document.getElementById("comment").select();
-			 document.getElementById("comment").focus();
-			 return false;
-		 }
-		 if (!checkEmail(email))
-		 {
-		 	alert('Email address is invalid');
-		 	return false;
-		 }
-		 return true;
-	}
-	 
-	function hideAllErrors() {
-	 	document.getElementById("nameError").style.display = "none";
-	 	document.getElementById("emailError").style.display = "none";
-	 	document.getElementById("commentError").style.display = "none";
-	}
-	 
-	function checkEmail(inputvalue) {
-	 	var pattern = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-	 	return pattern.test(inputvalue);
-	}
-
-	function checkForm() {
-	 	name = document.getElementById("name").value;
-	 	email = document.getElementById("email").value;
-	 	comment = document.getElementById("comment").value;
-	 
-		if (name == "")
-	 	{
-	 		hideAllErrors();
-			document.getElementById("nameError").style.display = "inline";
-			document.getElementById("name").select();
-			document.getElementById("name").focus();
-			return false;
-	 	}
-	 	else if (email == "")
-	 	{
-	 		hideAllErrors();
-	 		document.getElementById("emailError").style.display = "inline";
-	 		document.getElementById("email").select();
-	 		document.getElementById("email").focus();
-	 		return false;
-	 	}
-	 	else if (comment == "")
-	 	{
-	 		hideAllErrors();
-	 		document.getElementById("commentError").style.display = "inline";
-	 		document.getElementById("comment").select();
-	 		document.getElementById("comment").focus();
-	 		return false;
-	 	}
-	 	
-	 	if (!checkEmail(email))
-	 	{
-	 		alert('Email address is invalid');
-	 		return false;
-	 	}
+function checkForm() {
+	 name = document.getElementById("name").value;
+	 email = document.getElementById("email").value;
+	 comment = document.getElementById("comment").value;
+ 
+	 if (name == "")
+	 {
+	
+		 document.getElementById("nameError").style.display = "inline";
+		 document.getElementById("name").select();
+		 document.getElementById("name").focus();
+		 return false;
+	 }
+	 else if (email == "")
+	 {
+		 
+		 document.getElementById("emailError").style.display = "inline";
+		 document.getElementById("email").select();
+		 document.getElementById("email").focus();
+		 return false;
+	 }
+	 else if (comment == "")
+	 {
+		
+		 document.getElementById("commentError").style.display = "inline";
+		 document.getElementById("comment").select();
+		 document.getElementById("comment").focus();
+		 return false;
+	 }
+	 if (!checkEmail(email))
+	 {
+	 	alert('Email address is invalid');
+	 	return false;
+	 }
 	 return true;
-	}
-	 
-	function hideAllErrors() {
-		 document.getElementById("nameError").style.display = "none";
-		 document.getElementById("emailError").style.display = "none";
-		 document.getElementById("commentError").style.display = "none";
-	}
-	 
-	function checkEmail(inputvalue) {
-	 	var pattern = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-	 	return pattern.test(inputvalue);
+}
+ 
+function checkEmail(inputvalue) {
+ 	var pattern = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+ 	return pattern.test(inputvalue);
+}
+
+function checkForm() {
+ 	name = document.getElementById("name").value;
+ 	email = document.getElementById("email").value;
+ 	comment = document.getElementById("comment").value;
+ 
+	if (name == "")
+ 	{
+		document.getElementById("name").select();
+		document.getElementById("name").focus();
+		return false;
+ 	}
+ 	else if (email == "")
+ 	{
+ 		document.getElementById("email").select();
+ 		document.getElementById("email").focus();
+ 		return false;
+ 	}
+ 	else if (comment == "")
+ 	{
+ 		document.getElementById("comment").select();
+ 		document.getElementById("comment").focus();
+ 		return false;
+ 	}
+ 	
+ 	if (!checkEmail(email))
+ 	{
+ 		alert('Email address is invalid');
+ 		return false;
+ 	}
+ return true;
+}
+
+function checkEmail(inputvalue) {
+ 	var pattern = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+ 	return pattern.test(inputvalue);
+}
+*/
+
+////////////////////////////////////////////////////
+function formValidation(){
+
+	// Make quick references to our fields
+	var firstname =  document.getElementById('firstname');
+	var addr =  document.getElementById('addr');
+	var zip =  document.getElementById('zip');
+	var state =  document.getElementById('state');
+	var username =  document.getElementById('username');
+	var email =  document.getElementById('email');
+
+	//  to check empty form fields.
+
+	if(firstname.value.length == 0){
+		document.getElementById('head').innerText = "* All fields are mandatory *"; //this segment displays the validation rule for all fields
+		firstname.focus();
+		return false;
+	} 
+
+	// Check each input in the order that it appears in the form!
+	 if(inputAlphabet(firstname, "* For your name please use alphabets only *")){
+		
+		if(lengthDefine(username, 6, 8)){
+		    if(emailValidation(email, "* Please enter a valid email address *")){
+		        if(trueSelection(state, "* Please Choose any one option")){
+	                if(textAlphanumeric(addr, "* For Address please use numbers and letters *")){
+						if(textNumeric(zip, "* Please enter a valid zip code *")){
+						  return true;
+						}
+					}
+				}
+			}
+		}
 	}
 
+
+	return false;
+
+}
+
+//function that checks whether input text is numeric or not.
+
+function textNumeric(inputtext, alertMsg){
+var numericExpression = /^[0-9]+$/;
+if(inputtext.value.match(numericExpression)){
+	return true;
+}else{
+	document.getElementById('p6').innerText = alertMsg;  //this segment displays the validation rule for zip
+	inputtext.focus();
+	return false;
+}
+}
+
+
+//function that checks whether input text is an alphabetic character or not.
+
+function inputAlphabet(inputtext, alertMsg){
+var alphaExp = /^[a-zA-Z]+$/;
+if(inputtext.value.match(alphaExp)){
+	return true;
+}else{
+	document.getElementById('p1').innerText = alertMsg;  //this segment displays the validation rule for name
+	//alert(alertMsg);
+	inputtext.focus();
+	return false;
+}
+}
+
+
+//function that checks whether input text includes alphabetic and numeric characters.
+
+function textAlphanumeric(inputtext, alertMsg){
+var alphaExp = /^[0-9a-zA-Z]+$/;
+if(inputtext.value.match(alphaExp)){
+	return true;
+}else{
+	document.getElementById('p5').innerText = alertMsg; //this segment displays the validation rule for address
+	inputtext.focus();
+	return false;
+}
+}
+
+// Function that checks whether the input characters are restricted according to defined by user.
+
+function lengthDefine(inputtext, min, max){
+var uInput = inputtext.value;
+if(uInput.length >= min && uInput.length <= max){
+	return true;
+}else{
+	
+	document.getElementById('p2').innerText = "* Please enter between " +min+ " and " +max+ " characters *"; //this segment displays the validation rule for username
+	inputtext.focus();
+	return false;
+}
+}
+
+// Function that checks whether a option is selected from the selector and if it's not it displays an alert message.
+
+function trueSelection(inputtext, alertMsg){
+if(inputtext.value == "Please Choose"){
+	document.getElementById('p4').innerText = alertMsg; //this segment displays the validation rule for selection
+	inputtext.focus();
+	return false;
+}else{
+	return true;
+}
+}
+
+// Function that checks whether an user entered valid email address or not and displays alert message on wrong email address format.
+
+function emailValidation(inputtext, alertMsg){
+var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+if(inputtext.value.match(emailExp)){
+	return true;
+}else{
+	document.getElementById('p3').innerText = alertMsg; //this segment displays the validation rule for email
+	inputtext.focus();
+	return false;
+}
+}
 */
